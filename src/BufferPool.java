3e1712fdba6dacc1006af607eb72d5cc6cd161fd
Bob Bao <bob313@vt.edu>
2018-10-22 11:24:50 -0400

    all

--- src/BufferPool.java
+++ src/BufferPool.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -14,13 +15,33 @@ import java.util.Queue;
 @SuppressWarnings("unused")
 public class BufferPool implements BufferPoolADT {
     Queue<Integer> q = new LinkedList<>();
+    byte[] record = new byte[4];
+    long length = 0;
+    long recs = 0;
+    long count = 0;
+    byte[] key;
+    byte[] value;
 
 
+    /**
+     * constructs buffer pool
+     * 
+     * @param file
+     *            is the file to read from
+     * @throws IOException
+     *             in case file doesn't exist
+     */
     public BufferPool(String file) throws IOException {
         RandomAccessFile data = new RandomAccessFile(file, "rw");
-        long length = data.length();
-        long recs = length / 4096;
-        long cnt = 0;
+        length = data.length();
+        // div by 4, becuz multiple of 4096 bytes, but blocks hold 1024 records
+        recs = length / 4;
+// while (cnt < recs) {
+// cnt++;
+// data.readFully(record);
+// key = Arrays.copyOfRange(record, 0, 2);
+// value = Arrays.copyOfRange(record, 2, 4);
+// }
     }
 
 
