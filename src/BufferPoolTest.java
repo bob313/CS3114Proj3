2e4949830dba534e5b43a75f6f87551824bb5974
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-10-30 16:35:44 -0400

    Update BufferPoolTest.java

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -10,67 +10,46 @@ public class BufferPoolTest extends TestCase{
     private BufferPool bufPool;
     
     
-    public void testBufferBin() throws IOException {
-        FileGenerator generator = new FileGenerator();
-        String[] in = new String[3];
-        in[0] = "-b";
-        in[1] = "BinaryTest";
-        in[2] = "12288";
-        generator.generateFile(in);
+    public void testBufferASCII() throws IOException {
+        //FileGenerator generator = new FileGenerator();
+        //String[] in = new String[3];
+        //in[0] = "b";
+        //in[1] = "BinaryTest";
+        //in[2] = "3";
+        VirtualSort.generateFile("BinaryTest.txt", "3", "a");
         
-        bufPool = new BufferPool(in[1], 2);
+        bufPool = new BufferPool("BinaryTest.txt", 2);
         
-        Buffer temp = bufPool.acquireBuffer(1);
-        assertEquals(1, temp.getBlockNum());
+        Buffer temp = bufPool.acquireBuffer(2);
+        assertEquals(2, temp.getBlockNum());
         
-        Buffer temp2 = bufPool.acquireBuffer(0);
-        assertEquals(0, temp2.getBlockNum());
+        Buffer temp2 = bufPool.acquireBuffer(1);
+        assertEquals(1, temp2.getBlockNum());
         assertEquals(temp2.getBlockNum(), bufPool.getPool()[1].getBlockNum());
         
-        bufPool.acquireBuffer(1);
+        bufPool.acquireBuffer(2);
         assertEquals(temp.getBlockNum(), bufPool.getPool()[1].getBlockNum());
         
-        Buffer temp3 = bufPool.acquireBuffer(2);
-        assertEquals(2, temp3.getBlockNum());
+        Buffer temp3 = bufPool.acquireBuffer(3);
+        assertEquals(3, temp3.getBlockNum());
         assertEquals(temp3.getBlockNum(), bufPool.getPool()[1].getBlockNum());
         
         
         bufPool.getPool()[0].markDirty();
-        bufPool.acquireBuffer(0);
-        assertEquals(temp.getBlockNum(), bufPool.getPool()[1].getBlockNum());
+        bufPool.acquireBuffer(1);
+        assertEquals(temp2.getBlockNum(), bufPool.getPool()[1].getBlockNum());
         assertEquals(temp3.getBlockNum(), bufPool.getPool()[0].getBlockNum());
+        
+        //CLEAR TEST: Testing of the clearPool method begins here
+        bufPool.clearPool();
+        assertNull(bufPool.getPool()[0]);
+        assertNull(bufPool.getPool()[1]);
+        
         
     }
     
-    public void testBufferASCII() throws IOException {
-        FileGenerator generator = new FileGenerator();
-        String[] in = new String[3];
-        in[0] = "-a";
-        in[1] = "BinaryTest";
-        in[2] = "12288";
-        generator.generateFile(in);
-        
-        bufPool = new BufferPool(in[1], 2);
-        
-        Buffer temp = bufPool.acquireBuffer(1);
-        assertEquals(1, temp.getBlockNum());
-        
-        Buffer temp2 = bufPool.acquireBuffer(0);
-        assertEquals(0, temp2.getBlockNum());
-        assertEquals(temp2.getBlockNum(), bufPool.getPool()[1].getBlockNum());
-        
-        bufPool.acquireBuffer(1);
-        assertEquals(temp.getBlockNum(), bufPool.getPool()[1].getBlockNum());
-        
-        Buffer temp3 = bufPool.acquireBuffer(2);
-        assertEquals(2, temp3.getBlockNum());
-        assertEquals(temp3.getBlockNum(), bufPool.getPool()[1].getBlockNum());
-        
-        
-        bufPool.getPool()[0].markDirty();
-        bufPool.acquireBuffer(0);
-        assertEquals(temp.getBlockNum(), bufPool.getPool()[1].getBlockNum());
-        assertEquals(temp3.getBlockNum(), bufPool.getPool()[0].getBlockNum());
-        
-    }
+    
+    
+    
+    
 }
